3930f55a303edfe1c5b4c6e267faf446a2a77d4e
EL assoui achraf <achraf15elassoui@gmail.com>
2021-10-15 15:45:34 +0100

    f

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('openmobnave').addEventListener('click'  , ()=>{
+    document.getElementById('navitemsmobile').classList.add('open')
+})
+document.getElementById('closenmobnave').addEventListener('click'  , ()=>{
+    document.getElementById('navitemsmobile').classList.remove('open')
+})
+
+  
+
