3c71993278e0cc502293ad6aef9d041f41c66273
EL assoui achraf <achraf15elassoui@gmail.com>
2021-10-18 13:52:20 +0100

    fkkk

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ document.getElementById('openmobnave').addEventListener('click'  , ()=>{
 document.getElementById('closenmobnave').addEventListener('click'  , ()=>{
     document.getElementById('navitemsmobile').classList.remove('open')
 })
-
-  
+var lastScroll = 0
+addEventListener('scroll' , (e) => { 
+   
+    
+    let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;    if (currentScroll > 0 && lastScroll <= currentScroll){
+        lastScroll = currentScroll;
+        document.getElementsByClassName('nav')[0].classList.add('navoff')
+        document.getElementsByClassName('navmobile')[0].classList.add('navoff')
+        document.getElementsByClassName('navitemsmobile')[0].classList.add('navoff')
+    }else{
+        lastScroll = currentScroll;
+        document.getElementsByClassName('nav')[0].classList.remove('navoff')
+        document.getElementsByClassName('navmobile')[0].classList.remove('navoff')
+        document.getElementsByClassName('navitemsmobile')[0].classList.remove('navoff')
+    }
+    
+    
+})
 
